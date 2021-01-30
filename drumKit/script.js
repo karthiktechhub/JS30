5c5d02401f7eb2e6366b6a3a47b82322655399ed
Karthik K R <karthikkhoday29@gmail.com>
2021-01-30 15:41:21 +0530

    plays sound when clicking the btn

--- drumKit/script.js
+++ drumKit/script.js
@@ -1,6 +1,17 @@
 function playOnKey(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  playSound(audio,key);
+}
+
+function playOnClk() {
+  var key = this;
+  var data_key = key.getAttribute('data-key');
+  var audio = document.querySelector(`audio[data-key="${data_key}"]`);
+  playSound(audio,key);
+}
+
+function playSound(audio,key) {
   if (!audio) return; //stop the function from running.
   audio.currentTime = 0; //rewind to the start
   audio.play();
@@ -9,3 +20,8 @@ function playOnKey(e) {
 }
 
 window.addEventListener('keydown', playOnKey);
+
+const btn = document.querySelectorAll('.key');
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener('click',playOnClk);
+}
